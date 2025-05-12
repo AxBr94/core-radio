45a233ddfb5769b70502858ad2ec004f2e76b245
AxBr94 <axbr94@protonmail.com>
2025-05-12 14:21:10 +0300

    main script object-oriented now

--- app/public/javascript/script.js
+++ app/public/javascript/script.js
@@ -1,37 +1,52 @@
 'use strict';
 
-const audio = document.querySelector('#audio-tag');
-const playlists = document.querySelector('#playlists');
+class TrackLoader {
+    constructor(audioTagSelector, playlistsTagSelector) {
+        this.audio = audioTagSelector;
+        this.playlists = playlistsTagSelector;
+        this.audioUrl;
 
-const trackRequest = (playlist) => {
-    fetch(`http://127.0.0.1:3000/track?playlist=${playlist}`, {
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest'
+    }
+
+    setAudioUrl(blob) {
+        if (blob) {
+            this.audioUrl = URL.createObjectURL(blob);
         }
-      })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Audio load error.');
-        }
-        return response.blob();
-    })
-    .then(blob => {
-        const audioUrl = URL.createObjectURL(blob);
-        audio.setAttribute('src', `${audioUrl}`);
-        if (blob.type === 'audio/mpeg') {
-            audio.setAttribute('type', 'audio/mpeg');
-        } else if (blob.type === 'audio/ogg') {
-            audio.setAttribute('type', 'audio/ogg');
-        } else if (blob.type === 'audio/wav') {
-            audio.setAttribute('type', 'audio/wav');
-        }
-        audio.play();
-    })
-    .catch(err => console.error('Fetch error:', err));
+    }
+    
+    trackRequest(playlist) {
+        fetch(`http://127.0.0.1:3000/track?playlist=${playlist}`, {
+            headers: {
+              'X-Requested-With': 'XMLHttpRequest'
+            }
+          })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Audio load error.');
+            }
+            return response.blob();
+        })
+        .then(blob => {
+            this.setAudioUrl(blob);
+            this.audio.setAttribute('src', `${this.audioUrl}`);
+            if (blob.type === 'audio/mpeg') {
+                this.audio.setAttribute('type', 'audio/mpeg');
+            } else if (blob.type === 'audio/ogg') {
+                this.audio.setAttribute('type', 'audio/ogg');
+            } else if (blob.type === 'audio/wav') {
+                this.audio.setAttribute('type', 'audio/wav');
+            }
+            this.audio.play();
+        })
+        .catch(err => console.error('Fetch error:', err));
+    }
 }
 
-window.onload = trackRequest(playlists.value);
-
-playlists.addEventListener('change', () => {
-    trackRequest(playlists.value);
-});
+window.onload = () => {
+    const trackLoader = new TrackLoader(document.querySelector('#audio-tag'),
+        document.querySelector('#playlists'));
+    trackLoader.playlists.addEventListener('change', () => {
+        trackLoader.trackRequest(trackLoader.playlists.value);
+    });
+    trackLoader.trackRequest(document.querySelector('#playlists').value);//!
+}
