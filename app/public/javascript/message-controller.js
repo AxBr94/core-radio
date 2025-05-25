592fcf402bb22a77a5b7fc947aec4d21d7447cbc
AxBr94 <axbr94@protonmail.com>
2025-05-25 14:50:19 +0300

    chat is ready

--- app/public/javascript/message-controller.js
+++ app/public/javascript/message-controller.js
@@ -3,7 +3,8 @@
 class MessageController {
     constructor(messageList, userName, messageText) {
         this.output = messageList;
-        this.userName = this.setUserName(userName);
+        this.userName = document.cookie.replace('username=', '');
+        this.messageText = messageText;
         this.data = {
             userName: userName,
             message: messageText,
@@ -11,12 +12,17 @@ class MessageController {
         };
     }
 
-    renderMessages(messages) {
-        let li = document.createElement('li');
-        li.setAttribute('class', 'message');
-        for(let message of messages.slice(0, 8)) {
-            console.log(messages)
+    /* setUserName(userName) {
+        if(userName !== 'Anon') {
+            userName = 
+            return userName;
+        } 
+    } */
 
+    renderMessages(messages) {
+        for(let message of messages) {
+            let li = document.createElement('li');
+            li.setAttribute('class', 'message');
             li.innerHTML = `<article>
                 <p class="user-name">${message.userName}</p>
                 <p>
@@ -24,55 +30,51 @@ class MessageController {
                 </p>
                 <time>${message.date}</time>
             </article>`;
-            this.output.appendChild(li);       
-        }
-        if(messages[9]) {
-            li.innerHTML = `<article>
-                <p class="user-name">${messages[9].userName}</p>
-                <p>
-                    ${messages[9].message}
-                </p>
-                <time>${messages[9].date}</time>
-            </article>`;
             this.output.appendChild(li);
+            
+            if([...this.output.children].length > 10){
+                this.output.firstElementChild.remove();
+            }
         }
     }
 
     getMessages() {
         fetch('http://127.0.0.1:3000/chat/messages')
             .then(response => response.json())
-            .then(json => this.renderMessages(json));
+            .then(json => this.renderMessages(json))
+            .catch(error => console.error(error));
     }
 
     sendMessage() {
-        fetch('http://127.0.0.1:3000/chat', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.data)
-        })
-        .catch(error => console.error(error));
-    }
-    
-
-    setUserName(userName) {
-        if(userName !== 'Anon') {
-            userName = document.cookie.replace('username=', '');
-            return userName;
-        } 
+        if(this.messageText.length > 0 && this.userName.length > 0) {
+            fetch('http://127.0.0.1:3000/chat', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.data)
+            })
+            .then(() => {
+                document.querySelector('#message-text').value = "";
+            })
+            .catch(error => console.error(error));
+        } else {
+            alert('Your post is empty');
+        }
     }
 }
 
 const sendButton = document.querySelector('#send-button');
 
-sendButton.addEventListener('click', async () => {
+sendButton.addEventListener('click', () => {  
     const messageController = new MessageController(
         document.querySelector('#message-list'),
         document.querySelector('#user-name').value,
         document.querySelector('#message-text').value
     );
-    await setTimeout(messageController.sendMessage, 1);
-    await setTimeout(messageController.getMessages, 1);
+    messageController.sendMessage();
+    
+    setTimeout(() => {
+        messageController.getMessages();
+    }, 100);
 });
-
